68d46d5cfd672d3a456853bdfcd221d4ad0505e1
aleks2008-dev <sansanhc2008@tut.by>
2024-12-31 13:32:47 +0300

    add_all_endpoint

--- src/database.py
+++ src/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+import uuid
 
+from sqlalchemy import UUID
 from model import CategoryEnum
 
 engine = create_async_engine("postgresql+asyncpg://doctors:doctors@postgres-db/doctors", echo=True)
@@ -16,7 +18,15 @@ class Base(AsyncAttrs, DeclarativeBase):
 class DoctorORM(Base):
     __tablename__ = "doctors"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name: Mapped[str]
     age: Mapped[int]
     category: Mapped[CategoryEnum]
+
+# class DoctorORM(Base):
+#     __tablename__ = "doctors"
+#
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     name: Mapped[str]
+#     age: Mapped[int]
+#     category: Mapped[CategoryEnum]
--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
 from contextlib import asynccontextmanager
-from typing import Annotated, List
-from uuid import uuid4
-
-from model import CategoryEnum
-from fastapi import FastAPI, Depends, Query
+from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, text
+from sqlalchemy import select
 from database import engine, Base, DoctorORM, async_session
 from model import DoctorItem, DoctorItemCreate
+
+from sqlmodel import select
 
 
 @asynccontextmanager
@@ -31,42 +30,48 @@ async def get_session():
     async with async_session() as session:
         yield session
 
-@app.post("/doctors") # , response_model=DoctorItem
+@app.post("/doctors", response_model=DoctorItem)
 async def create_doctors(data: DoctorItemCreate, db: Annotated[AsyncSession, Depends(get_session)]):
     doctor = DoctorORM(name=data.name, age=data.age, category=data.category)
     db.add(doctor)
     await db.commit()
-
-
-@app.get("/doctors/{doctor_id}", response_model=DoctorItem)
-async def get_doctor_id(doctor_id: int):
-    print(doctor_id)
-    return {"id": uuid4(), "name": "aleks", "age": 23, "category": CategoryEnum.FIRST}
-
+    await db.refresh(doctor)
+    return DoctorItem(id=data.id, name=data.name, age=data.age, category=data.category)
 
 @app.get("/doctors/", response_model=list[DoctorItem])
-async def read_doctors(db: async_session = Depends(get_session)):
-    textual_sql = text("SELECT * FROM doctors")
-    orm_sql = select(DoctorORM).from_statement(textual_sql)
-    for user_obj in db.execute(orm_sql).scalars():
-        print(user_obj)
+async def read_doctors(db: Annotated[AsyncSession, Depends(get_session)]):
+        result = await db.execute(select(DoctorORM))
+        doctors = result.scalars().all()
+        return [DoctorItem(id=doctor.id, name=doctor.name, age=doctor.age, category=doctor.category) for doctor in doctors]
 
+@app.get("/doctors/{doctor_id}", response_model=DoctorItem)
+async def read_doctor(doctor_id: str, db: Annotated[AsyncSession, Depends(get_session)]):
+        result = await db.execute(select(DoctorORM).filter(DoctorORM.id == doctor_id))
+        doctor = result.scalars().first()
+        if not doctor:
+            raise HTTPException(status_code=404, detail="Doctor not found")
+        return DoctorItem(id=doctor.id, name=doctor.name, age=doctor.age, category=doctor.category)
 
+@app.patch("/doctors/{doctor_id}", response_model=DoctorItem)
+async def update_doctor(doctor_id: str, doctor: DoctorItem, db: Annotated[AsyncSession, Depends(get_session)]):
+        result = await db.execute(select(DoctorORM).filter(DoctorORM.id == doctor_id))
+        existing_doctor = result.scalars().first()
+        if not existing_doctor:
+            raise HTTPException(status_code=404, detail="Doctor not found")
 
-# @app.get("/doctors/", response_model=list[DoctorItem])
-# async def read_doctors(skip: int = 0, limit: int = 10, db: async_session = Depends(get_session)):
-#     return db.execute(select(DoctorORM).offset(skip).limit(limit)).all()
+        existing_doctor.name = doctor.name
+        existing_doctor.age = doctor.age
+        existing_doctor.category = doctor.category
+        await db.commit()
+        await db.refresh(existing_doctor)
+        return DoctorItem(id=doctor.id, name=doctor.name, age=doctor.age, category=doctor.category)
 
-# @app.get("/", response_model=List[DoctorItem])
-# def select_doctors():
-#     with async_session() as session:
-#         doctors = session.exec(select(DoctorORM)).all()
-#         session.commit()
-#         print(doctors.data)
-
-
-# @app.get("/", response_model=List[DoctorItem])
-# async def read_all_doctors(db: Annotated[AsyncSession, Depends(get_session)]):
-#     query = DoctorORM.select()
-#     return await db.fetch_all(query=query)
-
+@app.delete("/doctors/{doctor_id}", response_model=DoctorItem)
+async def delete_doctor(doctor_id: str, db: Annotated[AsyncSession, Depends(get_session)]):
+            result = await db.execute(select(DoctorORM).filter(DoctorORM.id == doctor_id))
+            doctor = result.scalars().first()
+            if not doctor:
+                raise HTTPException(status_code=404, detail="Doctor not found")
+            await db.delete(doctor)
+            await db.commit()
+            return DoctorItem(id=doctor.id, name=doctor.name, age=doctor.age, category=doctor.category)
